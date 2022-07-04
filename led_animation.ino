717d854f6de6c3bcf4c191c8f5ef269ef84478ad
Tom Andreas <tomvingaas@outlook.com>
2022-07-04 11:14:27 +0200

    arduino led script

---
+++ led_animation.ino
@@ -0,0 +1,101 @@
+#include <FastLED.h>
+#include <EEPROM.h>
+
+//WS2812 LED Strip
+//arduino
+
+#define NUM_LEDS 45
+#define DATA_PIN 10
+#define BTN_1 8
+#define HEARTBEATDELAY 5
+#define DEBOUNCE 100
+
+/* EEPROM Structure
+ *  0   animation
+ *  1   animation
+ *  2+  empty
+ */
+
+ int animationChoice;
+ int stateButton;
+ long debounce;
+ int brightness;
+
+ void setup() { 
+  //Setup uController
+  brightness = 84;
+  stateButton=HIGH;
+  debounce=0;
+
+  Serial.begin(57600);
+  pinMode(BTN_1, INPUT_PULLUP);
+  Serial.print("Neustart");
+  LEDS.addLeds<WS2812,DATA_PIN,RGB>(leds,NUM_LEDS);
+  LEDS.setBrightness(brightness);
+  animationChoice=readIntFromEEPROM(EEPROMADDR_ANIMATION);
+  checkAnimations();
+}
+
+void loop() { 
+  checkButton();//Button Checken
+  switch(animationChoice){
+    case 0: staticColor(0); break;
+    case 1: staticColor(64); break;
+    case 2: staticColor(96); break;     
+  }
+}
+
+void checkAnimations(){
+ if(animationChoice>20||animationChoice<0){
+    animationChoice=0;    
+  }
+  else if(animationChoice>13&&animationChoice<20){
+    animationChoice=20;    
+  }
+  else if(animationChoice>2&&animationChoice<10){
+    animationChoice=10;
+  }
+}
+
+bool checkButton(){
+    if((DEBOUNCE+debounce+DEBOUNCE)<millis()){
+    if(digitalRead(BTN_1)==LOW){
+      if(stateButton=HIGH){
+        animationChoice++;
+        checkAnimations();
+        writeIntIntoEEPROM(EEPROMADDR_ANIMATION, animationChoice);
+        Serial.print("BTN_1 - Animation: ");
+        Serial.print(animationChoice, DEC);
+        Serial.print("\n");
+        debounce=millis();        
+        stateButton=digitalRead(BTN_1);
+        return true;
+      }
+    }
+  }
+  stateButton=digitalRead(BTN_1);
+  return false;
+}
+
+void staticColor(int hue){
+  static int saturation=255;
+  static int direction=0;
+
+  for(int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CHSV(hue, 255, saturation);  
+  }
+  FastLED.show();
+  if(direction==0){
+    saturation--;
+  }
+  else{
+    saturation++;
+  }
+  if(saturation>=255){
+    direction = 0;
+  }
+  else if(saturation <=75){
+    direction =1;
+  }
+  delay(HEARTBEATDELAY);
+}
